3ca548cb129442e69347e4fa58ef6bafd3990775
Don-Vicks <shallangwavictorzakka@gmail.com>
2025-06-30 19:00:37 +0100

    Test route added

--- src/main.rs
+++ src/main.rs
@@ -96,12 +96,19 @@ async fn send_sol(req: web::Json<TransferRequest>) -> HttpResponse {
     HttpResponse::Ok().json(response)
 }
 
+// New hello route
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(generate_keypair)
             .service(send_sol)
+            .service(hello)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
